56f7a5d66fccc9552575346a4456de79f7b5b08e
Pedrodeforonda <pdeforonda@mail.austral.edu.ar>
2025-09-30 16:27:17 -0300

    Primer commit del microservicio print

---
+++ src/main/java/ingsis/printScriptManager/PrintScriptManagerApplication.java
@@ -0,0 +1,13 @@
+package ingsis.printScriptManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintScriptManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintScriptManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ingsis/printScriptManager/PrintScriptManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ingsis.printScriptManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintScriptManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
